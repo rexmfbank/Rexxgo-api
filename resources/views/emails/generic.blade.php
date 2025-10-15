a0c644a0a114fdc4815113f0d579bff54c58219d
logiccamp <logiccmpl@gmail.com>
2025-10-15 11:14:04 +0100

    removed login button

--- resources/views/emails/generic.blade.php
+++ resources/views/emails/generic.blade.php
@@ -4,7 +4,6 @@
     <!-- content -->
     <td valign="top" class="bodyContent" mc:edit="body_content">
         <p>{!!$msg!!}</p>
-        <a class="btn" href="{{env('APP_URL')}}">Login to your Account to view</a>
         <p>If you have any questions or need assistance getting started, please don't hesitate to reach out.</p>
     </td>
 @endsection
