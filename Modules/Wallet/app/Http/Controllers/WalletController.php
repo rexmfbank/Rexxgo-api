562209e392cb8bf3c1ea84f128186130afcf14f2
logiccamp <logiccmpl@gmail.com>
2025-11-26 04:07:56 +0100

    verify account number

--- Modules/Wallet/app/Http/Controllers/WalletController.php
+++ Modules/Wallet/app/Http/Controllers/WalletController.php
@@ -2662,7 +2662,7 @@ class WalletController extends Controller
             $data = $request->all();
             $data['borrower_id'] = auth()->guard('borrower')->user()->id;
             $response = $this->rexBank->VerifyAccountNumber($data);
-            Log::info($response);
+            Log::info($data);
             if ($response && isset($response['status']) && $response['status'] == 'success') {
                 return $this->success($response['data'], 'Acount verified.');
             } else {
